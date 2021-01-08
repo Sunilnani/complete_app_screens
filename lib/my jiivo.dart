fd82aeccf14ef81d5145a9632ac2239468238447
Sunilnani <42605686+Sunilnani@users.noreply.github.com>
2021-01-08 10:11:56 +0530

    updated1

--- lib/my jiivo.dart
+++ lib/my jiivo.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_app_screen/models/past.dart';
 import 'package:flutter_complete_app_screen/profile.dart';
 import 'models/current.dart';
 
@@ -293,7 +294,33 @@ class PastEvent extends StatefulWidget {
 }
 
 class _PastEventState extends State<PastEvent> {
-  int pressedButtonNo=0;
+  Welcoming listTodos = Welcoming();
+  bool _fetching = true;
+  void getHttp() async {
+    setState(() {
+      _fetching = true;
+    });
+    try {
+      Response response =
+      await Dio().get("https://networkintern.herokuapp.com/api/events?type=past");
+      setState(() {
+        listTodos = welcomingFromJson(jsonEncode(response.data));
+        _fetching = false;
+      });
+      print(response);
+    } catch (e) {
+      setState(() {
+        _fetching = false;
+      });
+      print(e);
+    }
+  }
+  int pressedButtonNo ;
+  @override
+  void initState() {
+    getHttp();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -330,243 +357,173 @@ class _PastEventState extends State<PastEvent> {
         ),
       ),
       body: Center(
-        child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
           child: Column(
             children: [
               SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 18),
-                child: Container(
-                  height: 265,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xFF00000008),
-                          offset: const Offset(
-                            0.0,
-                            3.0,
-                          ),
-                          blurRadius: 6.0,
-                        ), //BoxShadow
-                      ],
-                      border: Border.all(
-                        color: Colors.black12,
-                        width: 1,
-                      )
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Container(
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal:15,vertical: 8),
-                                child: Container(
-                                  height: 29,
-                                  width: 28,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(6),
-                                      border: Border.all(
-                                        color: Color(0xFFFF8701),
-                                        width: 2,
-                                      )
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text("22",style: TextStyle(color: Colors.red,fontSize: 11,fontWeight: FontWeight.w500),),
-                                      Text("Feb",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,fontSize: 6),)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  padding: EdgeInsets.only(right: 68),
-                                  child: Text("Google dev festival & meetup & Start up business ideas",style: TextStyle(color: Color(0xFF2A3E68),fontSize: 13,fontWeight: FontWeight.w500),),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right:8.0),
-                                child: Icon(Icons.favorite_border_outlined,color: Colors.orange,),
-                              )
-                            ],
-                          ),
-                        ),
-                        Image.asset("img/event.jpg"),
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-                            child: Text("Google Devfest",style: TextStyle(color: Color(0xFF2A3E68),fontSize: 16,fontWeight: FontWeight.w500),)
-                        ),
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-                            child: Text("10:30AM - 01:00PM  .  ITI, Mumbai...",style: TextStyle(color: Color(0xFFA1A0A0),fontSize: 13,fontWeight: FontWeight.w500),)
-                        ),
-                        Container(
-                          alignment: Alignment.bottomRight,
-                          padding: EdgeInsets.symmetric(horizontal: 8),
-                          child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left:230.0,),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/sunil.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:240),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/google.png"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:250),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/event.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:260),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/sunil.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:270),
-                                child: CircleAvatar(
-                                  child: Text("99+",style: TextStyle(color: Colors.white,fontSize: 8),),
-                                  backgroundColor: Colors.orange,
-                                  radius: 10,
-                                ),
-                              ),
+              _buildBody(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buildBody() {
+    if (_fetching) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    if (listTodos.events.length == 0) {
+      return Center(
+        child: Text("No Data"),
+      );
+    }
+    return Expanded(
+      child: ListView.builder(
+        itemCount: listTodos.events.length,
+        itemBuilder: (context, index) {
+          Welcoming todo = listTodos;
+          return CurrentEvents(
+            title: todo.events[index].title,
+            image: todo.events[index].bannerImage,
+            location: todo.events[index].location,
+          );
+        },
+      ),
+    );
+  }
+}
 
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
+class PastEvents extends StatelessWidget {
+  const PastEvents({
+    this.title,
+    this.image,
+    this.location,
+    Key key,
+  }) : super(key: key);
+  final String title;
+  final String image;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 18),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF00000008),
+                offset: const Offset(
+                  0.0,
+                  3.0,
                 ),
-              ),
-              SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 18),
-                child: Container(
-                  height: 265,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      border: Border.all(
-                        color: Colors.black12,
-                        width: 1,
-                      )
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal:15,vertical: 8),
-                              child: Container(
-                                height: 29,
-                                width: 28,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(6),
-                                    border: Border.all(
-                                      color: Color(0xFFFF8701),
-                                      width: 2,
-                                    )
-                                ),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text("28",style: TextStyle(color: Colors.red,fontSize: 11,fontWeight: FontWeight.w500),),
-                                    Text("Feb",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,fontSize: 6),)
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                padding: EdgeInsets.only(right: 68),
-                                child: Text("Standup Comdian by Varun laughter club shows",style: TextStyle(color: Color(0xFF2A3E68),fontSize: 13,fontWeight: FontWeight.w500),),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(right:8.0),
-                              child: Icon(Icons.favorite_border_outlined,color: Colors.orange,),
+                blurRadius: 6.0,
+              ), //BoxShadow
+            ],
+            border: Border.all(
+              color: Colors.black12,
+              width: 1,
+            )
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:15,vertical: 8),
+                      child: Container(
+                        height: 29,
+                        width: 28,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            border: Border.all(
+                              color: Color(0xFFFF8701),
+                              width: 2,
                             )
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("22",style: TextStyle(color: Colors.red,fontSize: 11,fontWeight: FontWeight.w500),),
+                            Text("Feb",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500,fontSize: 6),)
                           ],
                         ),
-                        Image.asset("img/event1.jpg"),
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-                            child: Text("Stand Up Comady",style: TextStyle(color: Color(0xFF2A3E68),fontSize: 16,fontWeight: FontWeight.w500),)
-                        ),
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-                            child: Text("08.30PM - 10.00PM . C N Vidhyalay,Ahmedabad.",style: TextStyle(color: Color(0xFFA1A0A0),fontSize: 13,fontWeight: FontWeight.w500),)
-                        ),
-                        Container(
-                          alignment: Alignment.bottomRight,
-                          padding: EdgeInsets.symmetric(horizontal: 8),
-                          child: Stack(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left:230.0,),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/sunil.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:240),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/google.png"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:250),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/event.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:260),
-                                child: CircleAvatar(
-                                  radius: 10,
-                                  backgroundImage: AssetImage("img/sunil.jpg"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left:270),
-                                child: CircleAvatar(
-                                  child: Text("99+",style: TextStyle(color: Colors.white,fontSize: 8),),
-                                  backgroundColor: Colors.orange,
-                                  radius: 10,
-                                ),
-                              ),
-
-                            ],
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(right: 68),
+                        child: Text(title,style: TextStyle(color: Color(0xFF2A3E68),fontSize: 13,fontWeight: FontWeight.w500),),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right:28.0),
+                      child: Icon(Icons.favorite_border_outlined,color: Colors.orange,),
+                    )
+                  ],
+                ),
+              ),
+              Image.asset(image),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+                  child: Text("Google Devfest",style: TextStyle(color: Color(0xFF2A3E68),fontSize: 16,fontWeight: FontWeight.w500),)
+              ),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+                  child: Text("10:30AM - 01:00PM  .  $location",style: TextStyle(color: Color(0xFFA1A0A0),fontSize: 13,fontWeight: FontWeight.w500),)
+              ),
+              Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left:230.0,),
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage("img/sunil.jpg"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left:240),
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage("img/google.png"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left:250),
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage("img/event.jpg"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left:260),
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundImage: AssetImage("img/sunil.jpg"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left:270),
+                      child: CircleAvatar(
+                        child: Text("99+",style: TextStyle(color: Colors.white,fontSize: 8),),
+                        backgroundColor: Colors.orange,
+                        radius: 10,
+                      ),
+                    ),
+
+                  ],
                 ),
               )
             ],
